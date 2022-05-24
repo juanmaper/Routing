86b42d3a036e9135da68b00d336b3debc4911284
Juanma <juanmaper30@gmail.com>
2022-05-24 17:03:39 +0200

    Test lifecycle component functions

--- src/modules/Pokemon/pages/AboutPage.vue
+++ src/modules/Pokemon/pages/AboutPage.vue
@@ -1,3 +1,67 @@
 <template>
   <h1>About Page</h1>
+  <hr>
+  <h2>{{ name }}</h2>
+  <button @click="onChangeName">
+    Change name
+  </button>
 </template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      name: 'Juanma'
+    }
+  },
+
+  methods: {
+    onChangeName() {
+      this.name = "Minerva"
+    }
+  },
+
+  beforeCreate() {
+      console.log('beforeCreate')
+  },
+  created() {
+      this.name = 'Juan Carlos'
+      console.log('created')
+      // Peticiones HTTP
+  },
+  beforeMount() {
+      console.log('beforeMount')
+  },
+  mounted() {
+      console.log('mounted')
+  },
+  beforeUpdate() {
+      console.log('beforeUpdate')
+  },
+  updated() {
+      console.log('updated')
+  },
+  activated() {
+      console.log('activated')
+  },
+  deactivated() {
+      console.log('deactivated')
+  },
+  beforeUnmount() {
+      console.log('beforeUnmount')
+  },
+  unmounted() {
+      console.log('unmounted')
+  },
+  errorCaptured() {
+      console.log('errorCaptured')
+  },
+  renderTracked() {
+      console.log('renderTracked')
+  },
+  renderTriggered() {
+      console.log('renderTriggered')
+  },
+}
+</script>
