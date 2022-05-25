b30cd7e3b503505b50dd2314c20055d989cadbcc
Juanma <juanmaper30@gmail.com>
2022-05-25 17:07:07 +0200

    Configure id by route params

--- src/modules/Pokemon/pages/PokemonPage.vue
+++ src/modules/Pokemon/pages/PokemonPage.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Pokemon Page</h1>
+  <h1>Pokemon: <span>#{{ id }}</span></h1>
 </template>
+
+<script>
+export default {
+  
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+
+  created() {
+    const { id } = this.$route.params;
+
+    console.log( id );
+    // this.id = id
+  }
+}
+</script>
