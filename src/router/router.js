94e409ee9d851ebeaecf121550166ffa9061aa1e
Juanma <juanmaper30@gmail.com>
2022-05-27 17:42:48 +0200

    Add layout for pokemon pages, url children

--- src/router/router.js
+++ src/router/router.js
@@ -3,27 +3,39 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/pokemon'
   },
-  { 
-    path: '/home', 
-    name: 'home',
-    component: () => import(/* webpackChunkName: "ListPage" */ '@/modules/Pokemon/pages/ListPage') 
+  {
+    path: '/pokemon',
+    name: 'pokemon',
+    component: () => import(/* webpackChunkName: "PokemonLayout" */ '@/modules/Pokemon/layouts/PokemonLayout'),
+    children: [
+      { 
+        path: 'home', 
+        name: 'pokemon-home',
+        component: () => import(/* webpackChunkName: "ListPage" */ '@/modules/Pokemon/pages/ListPage') 
+      },
+      { 
+        path: 'about', 
+        name: 'pokemon-about',
+        component: () => import(/* webpackChunkName: "AboutPage" */ '@/modules/Pokemon/pages/AboutPage')
+      },
+      { 
+        path: 'pokemonid/:id', 
+        name: 'pokemon-id',
+        component: () => import(/* webpackChunkName: "PokemonPage" */ '@/modules/Pokemon/pages/PokemonPage'),
+        props: ( route ) => {
+          const id = Number( route.params.id )
+          return isNaN( id ) ? { id: 1 } : { id }
+        }  
+      },
+      {
+        path: '',
+        redirect: { name: 'pokemon-about' }
+      }
+    ] 
   },
-  { 
-    path: '/about', 
-    name: 'about',
-    component: () => import(/* webpackChunkName: "AboutPage" */ '@/modules/Pokemon/pages/AboutPage')
-  },
-  { 
-    path: '/pokemonid/:id', 
-    name: 'pokemon-id',
-    component: () => import(/* webpackChunkName: "PokemonPage" */ '@/modules/Pokemon/pages/PokemonPage'),
-    props: ( route ) => {
-      const id = Number( route.params.id )
-      return isNaN( id ) ? { id: 1 } : { id }
-    }  
-  },
+ 
   { 
     path: '/:pathMatch(.*)*', 
     component: () => import(/* webpackChunkName: "NoPageFound" */ '@/modules/shared/pages/NoPageFound') 
